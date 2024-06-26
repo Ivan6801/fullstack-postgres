e4431d77de276986ede5d69dd4331cfafb061b95
Ivan Gonzalez <igonzalez@hospitalexcel.com>
2024-06-26 11:22:03 -0700

    Variables de ambiente en Node.js

--- backend/libs/postgres.pool.js
+++ backend/libs/postgres.pool.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
+const { config } = require('./../config/config');
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'ivan',
-  password: 'admin123',
-  database: 'wms',
-});
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
