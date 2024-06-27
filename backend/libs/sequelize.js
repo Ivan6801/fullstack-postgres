b7451f81d4cdcb4631fed60689d8a0242acfdeea
Ivan Gonzalez <igonzalez@hospitalexcel.com>
2024-06-27 15:16:45 -0700

    Cambiando la base de datos a MySQL

--- backend/libs/sequelize.js
+++ backend/libs/sequelize.js
@@ -4,8 +4,8 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const sequelize = new Sequelize(URI, { dialect: 'postgres', logging: true });
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const sequelize = new Sequelize(URI, { dialect: 'mysql', logging: true });
 
 setupModels(sequelize);
 
