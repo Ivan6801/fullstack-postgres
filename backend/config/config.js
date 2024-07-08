73572b8bd3d73026a45ca20f838e9125870e8488
Ivan Gonzalez <hal308366@gmail.com>
2024-07-07 18:55:50 -0700

    migrations add role

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,7 @@ const config = {
   port: process.env.PORT || 3001,
   dbUser: process.env.DB_USER || 'ivan',
   dbPassword: process.env.DB_PASSWORD || 'admin123',
-  dbHost: process.env.DB_HOST || 'postgres',
+  dbHost: process.env.DB_HOST || 'localhost',
   dbName: process.env.DB_NAME || 'wms',
   dbPort: process.env.DB_PORT || 5432,
 };
--- backend/db/migrations/20240701033131-add-role.js
+++ backend/db/migrations/20240701033131-add-role.js
@@ -3,8 +3,12 @@
 const { UserSchema, USER_TABLE } = require('./../models/user.model');
 
 module.exports = {
-  up: async (queryInterface) => {
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+  up: async (queryInterface, Sequelize) => {
+    const tableDefinition = await queryInterface.describeTable(USER_TABLE);
+
+    if (!tableDefinition.role) {
+      await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    }
   },
   down: async (queryInterface) => {
     await queryInterface.removeColumn(USER_TABLE, 'role');
